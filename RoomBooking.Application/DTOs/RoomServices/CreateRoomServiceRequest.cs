ed158b5444f85dfd6504cf23f8d8b1ac44dcb23d
Ayvako <ayvako2022@gmail.com>
2026-08-26 23:27:42 +0300

    Add room service request DTOs

---
+++ RoomBooking.Application/DTOs/RoomServices/CreateRoomServiceRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace RoomBooking.Application.DTOs.RoomServices;
+
+public class CreateRoomServiceRequest
+{
+    public string Name { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+}
---
+++ RoomBooking.Application/DTOs/RoomServices/UpdateRoomServiceRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace RoomBooking.Application.DTOs.RoomServices;
+
+public class UpdateRoomServiceRequest
+{
+    public string Name { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+}
