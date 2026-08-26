15a6d4af63a699a92b6c3f0deb113caa4d55c59d
Ayvako <ayvako2022@gmail.com>
2026-08-26 21:28:14 +0300

    Add Room entity configuration

---
+++ RoomBooking.Infrastructure/Persistence/Новая папка/RoomConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace RoomBooking.Infrastructure.Persistence.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RoomBooking.Domain.Entities;
+
+public class RoomConfiguration : IEntityTypeConfiguration<Room>
+{
+    public void Configure(EntityTypeBuilder<Room> builder)
+    {
+        builder.HasKey(room => room.Id);
+
+        builder.Property(room => room.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(room => room.Capacity)
+            .IsRequired();
+
+        builder.Property(room => room.BaseHourlyRate)
+            .IsRequired()
+            .HasPrecision(18, 2);
+    }
+}
