be9c5b11ae6a7fd574bc8db7fb653f0769db786c
Ayvako <ayvako2022@gmail.com>
2026-08-26 22:52:13 +0300

    Implement booking repository

---
+++ RoomBooking.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,58 @@
+﻿namespace RoomBooking.Infrastructure.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+using RoomBooking.Application.Interfaces;
+using RoomBooking.Domain.Entities;
+using RoomBooking.Infrastructure.Persistence;
+
+public class BookingRepository(RoomBookingDbContext context) : IBookingRepository
+{
+    public async Task<Booking?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await context.Bookings
+            .Include(booking => booking.Room)
+            .ThenInclude(room => room.Services)
+            .FirstOrDefaultAsync(
+                booking => booking.Id == id,
+                cancellationToken);
+    }
+
+    public async Task<IReadOnlyList<Booking>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await context.Bookings
+            .Include(booking => booking.Room)
+            .ThenInclude(room => room.Services)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<IReadOnlyList<Booking>> GetByRoomAndPeriodAsync(Guid roomId, DateTime startTime, DateTime endTime, CancellationToken cancellationToken = default)
+    {
+        return await context.Bookings
+            .Where(booking =>
+                booking.RoomId == roomId &&
+                booking.StartTime < endTime &&
+                booking.EndTime > startTime)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task AddAsync(Booking booking, CancellationToken cancellationToken = default)
+    {
+        await context.Bookings.AddAsync(booking, cancellationToken);
+
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task UpdateAsync(Booking booking, CancellationToken cancellationToken = default)
+    {
+        context.Bookings.Update(booking);
+
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task DeleteAsync(Booking booking, CancellationToken cancellationToken = default)
+    {
+        context.Bookings.Remove(booking);
+
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
