c39dad105098f8d0a55d972dbd7dd532a778d425
Ayvako <ayvako2022@gmail.com>
2026-08-26 21:12:28 +0300

    Add RoomBooking DbContext

---
+++ RoomBooking.Infrastructure/Persistence/RoomBookingDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace RoomBooking.Infrastructure.Persistence;
+
+using Microsoft.EntityFrameworkCore;
+using RoomBooking.Domain.Entities;
+
+public class RoomBookingDbContext(DbContextOptions<RoomBookingDbContext> options) : DbContext(options)
+{
+    public DbSet<Room> Rooms => this.Set<Room>();
+
+    public DbSet<RoomService> RoomServices => this.Set<RoomService>();
+
+    public DbSet<Booking> Bookings => this.Set<Booking>();
+}
