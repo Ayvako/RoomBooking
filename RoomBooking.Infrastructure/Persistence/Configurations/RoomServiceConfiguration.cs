5bde05690b3cfc69464e6c0245329f475e409da6
Ayvako <ayvako2022@gmail.com>
2026-08-26 22:16:11 +0300

    Add RoomService entity configuration

---
+++ RoomBooking.Infrastructure/Persistence/Configurations/RoomServiceConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace RoomBooking.Infrastructure.Persistence.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RoomBooking.Domain.Entities;
+
+public class RoomServiceConfiguration : IEntityTypeConfiguration<RoomService>
+{
+    public void Configure(EntityTypeBuilder<RoomService> builder)
+    {
+        builder.HasKey(service => service.Id);
+
+        builder.Property(service => service.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(service => service.Price)
+            .IsRequired()
+            .HasPrecision(18, 2);
+
+        builder.HasOne(service => service.Room)
+            .WithMany(room => room.Services)
+            .HasForeignKey(service => service.RoomId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
