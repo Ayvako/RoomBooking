e4960462c9a63a941994c241f14c92a5b59e232d
Ayvako <ayvako2022@gmail.com>
2026-08-26 22:44:24 +0300

    Implement room repository

---
+++ RoomBooking.Infrastructure/Repositories/RoomRepository.cs
@@ -0,0 +1,43 @@
+﻿namespace RoomBooking.Infrastructure.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+using RoomBooking.Application.Interfaces;
+using RoomBooking.Domain.Entities;
+using RoomBooking.Infrastructure.Persistence;
+
+public class RoomRepository(RoomBookingDbContext context) : IRoomRepository
+{
+    public async Task<Room?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await context.Rooms
+            .Include(room => room.Services)
+            .FirstOrDefaultAsync(
+                room => room.Id == id,
+                cancellationToken);
+    }
+
+    public async Task<IReadOnlyList<Room>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await context.Rooms
+            .Include(room => room.Services)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task AddAsync(Room room, CancellationToken cancellationToken = default)
+    {
+        await context.Rooms.AddAsync(room, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task UpdateAsync(Room room, CancellationToken cancellationToken = default)
+    {
+        context.Rooms.Update(room);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task DeleteAsync(Room room, CancellationToken cancellationToken = default)
+    {
+        context.Rooms.Remove(room);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
